992233dfda82e7540a97c5310809d2857439be7c
foash-111 <mr2221@fayoum.edu.eg>
2024-08-01 16:10:34 +0300

    type-annotated  sum_list function

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-type-annotated function sum_list which takes a list input_list of floats as argument and returns their sum as a float.
+type-annotated function sum_list which takes a list
+input_list of floats as argument and returns their
+sum as a float.
 """
 
 
