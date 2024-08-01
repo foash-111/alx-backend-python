0e454526bbf2964a393c98e058768f23162f0719
foash-111 <mr2221@fayoum.edu.eg>
2024-08-01 15:09:52 +0300

    type-annotated variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" 
+"""
 Define and annotate the following variables
 with the specified values:
 
@@ -9,6 +9,7 @@ i_understand_annotations,
 a boolean with a value of True
 school, a string with a value of “Holberton”
 """
+
 
 a: int = 1
 pi: float = 3.14
