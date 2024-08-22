3d98b938bfc810ae50c3c416381177780a40a0fb
foash-111 <mr2221@fayoum.edu.eg>
2024-08-22 14:12:21 +0300

    just return callable ./4-main.py !

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,8 +3,9 @@
 
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
 from time import perf_counter
+task_wait_random = __import__('3-tasks').task_wait_random
+
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """return a list of length n, filled by max_delay range"""
