23133246ae7ea2a1e11394b63d56b7b294cc076e
foash-111 <mr2221@fayoum.edu.eg>
2024-08-30 16:36:01 +0300

    unittest

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,3 +40,44 @@ class TestGithubOrgClient(unittest.TestCase):
 
 # new_callable=PropertyMock:
 # This tells patch.object to create a mock specifically for a property.
+
+    @patch('client.get_json')  # Step 1: Mock the get_json function
+    def test_public_repos(self, get_mock):
+        """follow the steps"""
+        repos = [  # Step 2: Define the fake repository data
+            {'name': 'google'},
+            {'name': 'abc'}
+        ]
+        get_mock.return_value = repos
+        # Step 3: Set the mock to return this data
+
+        with patch.object(
+                GithubOrgClient, '_public_repos_url', new_callable=PropertyMock
+                ) as mock_url:
+            mock_url.return_value = 'https://www.google.co.uk/'
+            # Step 4: Mock the _public_repos_url property
+
+            client = GithubOrgClient('google')
+            # Create an instance of GithubOrgClient
+            result = client.public_repos()
+            # Call the public_repos method
+
+            self.assertEqual(result, ['google', 'abc'])
+            # Step 5: Check if the result matches the expected list
+            mock_url.assert_called_once()
+            # Ensure _public_repos_url was called exactly once
+            get_mock.assert_called_once_with('https://www.google.co.uk/')
+            # Ensure get_json was called with the correct URL
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """check if has licence or not"""
+        client = GithubOrgClient('admin')
+        self.assertEqual(client.has_license(repo, license_key), expected)
+
+
+# Use @patch as a decorator to mock get_json
+# Use patch as a context manager to mock
